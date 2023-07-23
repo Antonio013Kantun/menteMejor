79b10ee39634b5aeabde8d2f8c3a45243716d1f8
eduardo87DR <spixerspi@gmail.com>
2023-07-23 15:12:44 -0500

    imagen corregida

--- src/Dashboard-page/pages/products/addProducts.jsx
+++ src/Dashboard-page/pages/products/addProducts.jsx
@@ -9,7 +9,6 @@ export default function AddProducts() {
     precio: "",
     stock: "",
     imagen: "",
-    
   });
 
   const handleChange = function (e) {
@@ -42,52 +41,52 @@ export default function AddProducts() {
 
   return (
     <div className="flex justify-center items-center h-screen">
-    <div className="md:w-1/3 lg:w-2/1">
-      <table className="w-full bg-white border border-gray-300 rounded-lg">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 bg-gray-100 border-b border-gray-300 text-left">Insertar producto</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="py-4 px-6">
-              <Form>
-                <Form.Group className="mb-3" controlId="nombre">
-                  <Form.Label>Nombre</Form.Label>
-                  <Form.Control type="text" placeholder="Nombre del producto" name="nombre"  onChange={handleChange}/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="descripcion">
-                  <Form.Label>Descripción</Form.Label>
-                  <Form.Control as="textarea" rows={3} placeholder="Descripción del producto" name="descripcion"  onChange={handleChange}/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="precio">
-                  <Form.Label>Precio</Form.Label>
-                  <Form.Control type="number" placeholder="Precio del producto" name="precio" onChange={handleChange} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="stock">
-                  <Form.Label>Stock</Form.Label>
-                  <Form.Control type="number" placeholder="Stock del producto" name="stock" onChange={handleChange} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="imagen">
-                  <Form.Label>Imagen</Form.Label>
-                  <Form.Control type="file" placeholder="url de la imagen del producto" name="imagen"  onChange={handleChange} />
-                </Form.Group>
-                <div className="flex justify-center">
-                  <button
-                    type="submit"
-                    onClick={handleSubmit}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Enviar
-                  </button>
-                </div>
-              </Form>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="md:w-1/3 lg:w-2/1">
+        <table className="w-full bg-white border border-gray-300 rounded-lg">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 bg-gray-100 border-b border-gray-300 text-left">Insertar producto</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="py-4 px-6">
+                <Form>
+                  <Form.Group className="mb-3" controlId="nombre">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control type="text" placeholder="Nombre del producto" name="nombre" onChange={handleChange} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="descripcion">
+                    <Form.Label>Descripción</Form.Label>
+                    <Form.Control as="textarea" rows={3} placeholder="Descripción del producto" name="descripcion" onChange={handleChange} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="precio">
+                    <Form.Label>Precio</Form.Label>
+                    <Form.Control type="number" placeholder="Precio del producto" name="precio" onChange={handleChange} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="stock">
+                    <Form.Label>Stock</Form.Label>
+                    <Form.Control type="number" placeholder="Stock del producto" name="stock" onChange={handleChange} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="imagen">
+                    <Form.Label>Imagen</Form.Label>
+                    <Form.Control type="text" placeholder="URL de la imagen del producto" name="imagen" onChange={handleChange} />
+                  </Form.Group>
+                  <div className="flex justify-center">
+                    <button
+                      type="submit"
+                      onClick={handleSubmit}
+                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Enviar
+                    </button>
+                  </div>
+                </Form>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div> 
   );
 }
